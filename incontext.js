f88e833acbb716dd280cfa1f17e8753295aef30f
Nitin Borwankar <nborwankar@gmail.com>
2014-12-27 20:44:11 -0800

    Initial commit

---
+++ incontext.js
@@ -0,0 +1,26 @@
+/**
+ * Created by nitin on 12/20/14.
+ */
+
+/* get config */
+/* test connectivity or warn and exit */
+/* pull current context core */
+/* display it */
+/* pull current context periphery */
+/* lazy display it */
+
+/**
+ * get a sample of 15 stories
+ * pull all PvTr JSON into a single attr called stories
+ * pull all JIRA JSON
+ * pull all P4 JSON
+ * pull all git JSON
+ * pull all info from emails
+ * */
+
+
+
+
+
+
+
